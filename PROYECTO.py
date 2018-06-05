7aeae5e493494f6a7f38e456d868c5e2c9a98f54
christianledgard <39962750+christianledgard@users.noreply.github.com>
2018-06-05 12:55:10 -0500

    Add files via upload

---
+++ PROYECTO.py
@@ -0,0 +1,5 @@
+print("Bienvenidos al programa: Aprendiendo en Python")
+print("- - - - - - - - - - - - - - - - - - - - - - -")
+print("Escribe tu nombre a continuación para ingresar al programa:")
+usuarios = {}
+usuarios.update({input("Nombre: "):"0"})
