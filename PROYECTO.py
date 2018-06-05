f2d0d3beff440a275dbfe15955fd765f02b62563
Christian Ledgard <christianledgard@gmail.com>
2018-06-05 14:10:31 -0500

    Agregando el Menu

--- PROYECTO.py
+++ PROYECTO.py
@@ -1,5 +1,22 @@
 print("Bienvenidos al programa: Aprendiendo en Python")
+print()
 print("- - - - - - - - - - - - - - - - - - - - - - -")
+print()
 print("Escribe tu nombre a continuación para ingresar al programa:")
-usuarios = {}
-usuarios.update({input("Nombre: "):"0"})
+
+usuarioactual = input("Nombre: ")
+
+usuario = {}
+usuario.update({usuarioactual:0})
+
+#Ingresa al Menu del Programa
+print("\n"*20)
+print(f"--- Bienvenido al menu {usuarioactual} ---" )
+print("\n")
+print("1. Acerca de...")
+print("2. Mantenimiento de Usuarios...")
+print("3. Rankings")
+print("4. Empezar el cuso de Python")
+print("5. Ayuda")
+print()
+sel = input("Elija una opción (1-5): ")
